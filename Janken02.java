2f5178f815883b2602a34dcc5224bd7ec3634c74
momonoir <momoko.hattori.17@gmail.com>
2017-10-03 15:33:22 +0900

    3.前回の修正、3本先取パターンは完成
    while文の外に、cpuに乱数を代入する分を書いてしまっていたのが不具合の原因だった
    点数加算の文を修正、ゲーム終了時の表示を追記

--- Janken02.java
+++ Janken02.java
@@ -1,34 +1,31 @@
 public class Janken02 {
   public static void main(String[] args) {
-//cpuの選択を乱数で決める
-    int cpu = new java.util.Random().nextInt(3) +1;
-    int youWin = 0;
-    int cpuWin = 0;
+    int cpu; //cpuの選択
+    int youWin = 0; //プレイヤーの勝ち点
+    int cpuWin = 0; //cpuの勝ち点
+    String result = "あいこ"; //初期値
 
-    //ゲームの開始、プレイヤーが選択するまで
+    //ゲームの開始
     System.out.println("じゃんけんゲーム");
     System.out.println("3本先取です");
 
     //勝敗判定
-    String result = "あいこ"; //初期値
-
-    while (youWin < 3 && cpuWin < 3) {
-      System.out.println("相手の選択の確認" + cpu);
+    while (youWin < 3 && cpuWin < 3) { //どちらかが3点取るまで
+      cpu = new java.util.Random().nextInt(3) +1; //cpuが何を出すか乱数で決定
       System.out.println("数字を入力してください");
       System.out.print("1:グー  2:チョキ  3:パー \n");
       //キーボードから入力を受け付ける
       int you = new java.util.Scanner(System.in).nextInt();
-      //１：グー、２：チョキ、３：パー
+
+      //cpuが出した手で勝敗を判定
       if (cpu != you) { //あいこでないなら
         switch (cpu) {
           case 1: //pcがグー
             System.out.println("相手：グー");
             if (you == 2) {
               result = "相手の勝ち";
-              cpuWin++;
               } else if (you == 3) {
                 result = "あなたの勝ち";
-                youWin++;
               }
               break;
 
@@ -36,10 +33,8 @@ public class Janken02 {
             System.out.println("相手：チョキ");
             if (you == 3) {
               result = "相手の勝ち";
-              cpuWin++;
               } else if (you == 1) {
               result = "あなたの勝ち";
-              youWin++;
               }
               break;
 
@@ -47,18 +42,29 @@ public class Janken02 {
             System.out.println("相手：パー");
             if (you == 1) {
               result = "相手の勝ち";
-              cpuWin++;
               } else if (you == 2) {
               result = "あなたの勝ち";
-              youWin++;
               }
               break;
             }
           }
+
+      //勝ったほうに1点加算
+      if (result.equals("あなたの勝ち")) {
+        youWin++;
+      } else if (result.equals("相手の勝ち")) {
+        cpuWin++;
+      }
+
+      //結果と現在の点数を表示
       System.out.println(result);
       System.out.println("現在の点数");
-      System.out.println("あなた：" + youWin + "-" + "相手：" + cpuWin);
+      System.out.println("あなた：" + youWin + "  " + "相手：" + cpuWin);
     }
+
+    //ゲームの終了
+    System.out.println("\n勝負あり！！");
+    System.out.println(youWin + " 対 " + cpuWin + " で " + result + "です");
   }
 }
 
